8781a063cf1358c2c2a5010e519d855d79e50a4b
Gary Lloyd <garfy70@hotmail.com>
2025-05-08 12:54:08 +0100

    Initial commit

---
+++ restaurantAPI/Model/Restaurant.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace restaurantAPI.Model
+{
+    public class Restaurant
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+        public required string Phone { get; set; }
+    }
+}
